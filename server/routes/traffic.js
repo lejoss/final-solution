a3e8b74d737f2962c8f43ec017a1af10b8401617
lejoss <nativo.alejandro@gmail.com>
2016-03-29 12:32:07 -0500

    added server

---
+++ server/routes/traffic.js
@@ -0,0 +1,19 @@
+/**
+ * Created by lejoss on 3/29/16.
+ */
+
+var express      = require('express');
+var trafficModel = require('../models/traffic');
+var router       = express.Router();
+
+
+router.post('/app/traffic-model', function(req, res) {
+    var entry = req.body;
+    trafficModel.createDriverEntry(entry, function (result) {
+        req.body.id = result.rows[0].id;
+        res.status(201);
+        res.send(req.body);
+    })
+});
+
+module.exports = router;
